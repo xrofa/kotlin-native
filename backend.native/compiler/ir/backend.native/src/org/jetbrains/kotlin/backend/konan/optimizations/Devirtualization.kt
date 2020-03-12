b5e2b623508559f7c3f7eaa8496c385ee4ad726a
Igor Chevdar <igor.chevdar@jetbrains.com>
2020-03-12 17:43:56 +0300

    Moved IntArrayList & LongArrayList to a separate file

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/optimizations/Devirtualization.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/optimizations/Devirtualization.kt
@@ -13,6 +13,8 @@ import org.jetbrains.kotlin.backend.common.push
 import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
 import org.jetbrains.kotlin.backend.common.lower.irBlock
 import org.jetbrains.kotlin.backend.konan.*
+import org.jetbrains.kotlin.backend.konan.util.IntArrayList
+import org.jetbrains.kotlin.backend.konan.util.LongArrayList
 import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.builders.*
@@ -35,7 +37,6 @@ import org.jetbrains.kotlin.ir.visitors.IrElementVisitorVoid
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
 import org.jetbrains.kotlin.name.Name
-import sun.misc.Unsafe
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -128,130 +129,6 @@ internal object Devirtualization {
         private val entryPoint = context.ir.symbols.entryPoint?.owner
 
         private val symbolTable = moduleDFG.symbolTable
-
-        class IntArrayList : Iterable<Int> {
-            private var array = IntArray(3)
-            private var length = 0
-
-            val size get() = length
-
-            fun isEmpty() = size == 0
-
-            fun add(x: Int) {
-                length++
-                ensureCapacity(length)
-                set(length - 1, x)
-            }
-
-            fun reserve(minSize: Int) {
-                if (length >= minSize) return
-                length = minSize
-                ensureCapacity(minSize)
-            }
-
-            operator fun get(index: Int): Int = when {
-                index < 0 || index >= length -> throw IndexOutOfBoundsException("Index out of range: $index")
-
-                else -> array[index]
-            }
-
-            operator fun set(index: Int, x: Int) = when {
-                index < 0 || index >= length -> throw IndexOutOfBoundsException("Index out of range: $index")
-
-                else -> array[index] = x
-            }
-
-            override operator fun iterator(): Iterator<Int> = Itr()
-
-            private fun ensureCapacity(minCapacity: Int) {
-                val oldArray = array
-                if (minCapacity > oldArray.size) {
-                    var newSize = oldArray.size * 3 / 2
-                    if (minCapacity > newSize)
-                        newSize = minCapacity
-                    val newArray = IntArray(newSize)
-                    TheUnsafe.copyMemory(oldArray, IntArrayDataOffset, newArray, IntArrayDataOffset, oldArray.size.toLong() * 4)
-                    array = newArray
-                }
-            }
-
-            private inner class Itr : Iterator<Int> {
-                private var index = 0
-
-                override fun hasNext() = index < size
-
-                override fun next() = get(index++)
-
-            }
-
-            companion object {
-                val TheUnsafe = Unsafe::class.java.getDeclaredField("theUnsafe").let {
-                    it.isAccessible = true
-                    it.get(null) as Unsafe
-                }
-
-                val IntArrayDataOffset = TheUnsafe.arrayBaseOffset(IntArray::class.java).toLong()
-            }
-        }
-
-        class LongArrayList : Iterable<Long> {
-            private var array = LongArray(3)
-            private var length = 0
-
-            val size get() = length
-
-            fun isEmpty() = size == 0
-
-            fun add(x: Long) {
-                length++
-                ensureCapacity(length)
-                set(length - 1, x)
-            }
-
-            operator fun get(index: Int): Long = when {
-                index < 0 || index >= length -> throw IndexOutOfBoundsException("Index out of range: $index")
-
-                else -> array[index]
-            }
-
-            operator fun set(index: Int, x: Long) = when {
-                index < 0 || index >= length -> throw IndexOutOfBoundsException("Index out of range: $index")
-
-                else -> array[index] = x
-            }
-
-            override operator fun iterator(): Iterator<Long> = Itr()
-
-            private fun ensureCapacity(minCapacity: Int) {
-                val oldArray = array
-                if (minCapacity > oldArray.size) {
-                    var newSize = oldArray.size * 3 / 2
-                    if (minCapacity > newSize)
-                        newSize = minCapacity
-                    val newArray = LongArray(newSize)
-                    TheUnsafe.copyMemory(oldArray, LongArrayDataOffset, newArray, LongArrayDataOffset, oldArray.size.toLong() * 8)
-                    array = newArray
-                }
-            }
-
-            private inner class Itr : Iterator<Long> {
-                private var index = 0
-
-                override fun hasNext() = index < size
-
-                override fun next() = get(index++)
-
-            }
-
-            companion object {
-                val TheUnsafe = Unsafe::class.java.getDeclaredField("theUnsafe").let {
-                    it.isAccessible = true
-                    it.get(null) as Unsafe
-                }
-
-                val LongArrayDataOffset = TheUnsafe.arrayBaseOffset(LongArray::class.java).toLong()
-            }
-        }
 
         sealed class Node(val id: Int) {
             var directCastEdges: MutableList<CastEdge>? = null
